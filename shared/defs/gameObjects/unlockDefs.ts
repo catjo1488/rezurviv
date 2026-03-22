ad2e0fa8899c4fca05d75f26e861f0b4611b52f8
d3677 <d36774671@gmail.com>
2026-03-22 20:52:47 +0800

    fix

--- shared/defs/gameObjects/unlockDefs.ts
+++ shared/defs/gameObjects/unlockDefs.ts
@@ -14,6 +14,9 @@ const _allowedMeleeSkins = [
     "huntsman_burnished",
     "bowie_vintage",
     "bowie_frontier",
+    "lasr_swrd",
+    "lasr_swrd_02",
+    "lasr_swrd_03",
 ];
 const _allowedOutfits = [
     "outfitBase",
